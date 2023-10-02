9f00febbf79633273803bc54abd3a29c3da89814
tofubeat <111270992+tofubeat@users.noreply.github.com>
2023-10-02 11:44:51 +0900

    Squashed commit of the following:
    commit 9ea03c396748e3031078654d63a14fe86e91793b
    Author: tofubeat <111270992+tofubeat@users.noreply.github.com>
    Date:   Mon Oct 2 11:44:22 2023 +0900
    
        [Fix] Wishlist sql Error 수정
    
        Wishlist sql Error 및 ProfessorCourse sql Error 수정

--- src/main/java/com/kosa/ucr/registration/dto/Registration.java
+++ src/main/java/com/kosa/ucr/registration/dto/Registration.java
@@ -1,18 +1,18 @@
-//package com.kosa.ucr.registration.dto;
-//
-//import java.util.List;
-//
-//import com.kosa.ucr.course.dto.Course;
-//import com.kosa.ucr.user.dto.Student;
-//
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//@NoArgsConstructor @AllArgsConstructor
-//@Setter @Getter
-//public class Registration {
-//	List<Course> courses;
-//	Student stuId;
-//}
+package com.kosa.ucr.registration.dto;
+
+import java.util.List;
+
+import com.kosa.ucr.course.dto.Course;
+import com.kosa.ucr.user.dto.Student;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+@Setter @Getter
+public class Registration {
+	List<Course> courses;
+	Student stuId;
+}
