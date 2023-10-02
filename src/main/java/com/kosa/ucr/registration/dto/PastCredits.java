4dfea638ee56e15944246638e40c0eaf3055bcd6
tofubeat <111270992+tofubeat@users.noreply.github.com>
2023-10-02 18:00:47 +0900

    [Feat] PastCredits.dto 추가
    과거학점 목록을 보여주기 위하여
     com.kosa.ucr.registration.dto에 PastCredits.dto추가함

---
+++ src/main/java/com/kosa/ucr/registration/dto/PastCredits.java
@@ -0,0 +1,14 @@
+package com.kosa.ucr.registration.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor
+@Setter @Getter
+public class PastCredits {
+	private int stuId;
+	private String division;
+	private int credit;
+}
